92cc5dd4c100ec5b0fda2f7696fea3da82a61b9b
varaprasad9010-cloud <83047546+varaprasad9010-cloud@users.noreply.github.com>
2026-07-14 15:18:03 +0530

    Create arthametic-operation.py

---
+++ src/arthametic-operation.py
@@ -0,0 +1,15 @@
+# Define the numbers
+num1 = 7
+num2 = 21
+
+# Perform operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+# Display the results
+print(f"Addition ({num1} + {num2}): {addition}")
+print(f"Subtraction ({num1} - {num2}): {subtraction}")
+print(f"Multiplication ({num1} * {num2}): {multiplication}")
+print(f"Division ({num1} / {num2}): {division}")
